6f5f433446dc7203de3abee58bd989df670a2860
Maxim <the.buxta@yandex.ru>
2022-02-04 16:38:36 +0300

    complete task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,1 +1,15 @@
 ﻿// Задача 4: Найти максимальное из трёх чисел.
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if (b > a)
+    {
+    max = b;
+    }
+if (c > max)
+    {
+    max = c;
+    }
+Console.WriteLine("Максимально число " + max);
