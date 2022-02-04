834b78c2298ebedf8b4ab952e73d15d4bd2dc84a
Maxim <the.buxta@yandex.ru>
2022-02-04 16:50:19 +0300

    complete task6

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Задача 6: Выяснить является ли число чётным
+int a = int.Parse(Console.ReadLine());
+int b = a%2;
+if (b == 0)
+{
+    Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
