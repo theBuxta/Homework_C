b17d95a58254a361b7616a44d5ecc6dde921b239
Maxim <the.buxta@yandex.ru>
2022-02-04 19:13:35 +0300

    edited and add .gitignore

--- task4/Program.cs
+++ task4/Program.cs
@@ -2,14 +2,14 @@
 int a = int.Parse(Console.ReadLine());
 int b = int.Parse(Console.ReadLine());
 int c = int.Parse(Console.ReadLine());
-
 int max = a;
+
 if (b > a)
-    {
+{
     max = b;
-    }
+}
 if (c > max)
-    {
+{
     max = c;
-    }
+}
 Console.WriteLine("Максимально число " + max);
--- task6/Program.cs
+++ task6/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Задача 6: Выяснить является ли число чётным
+
 int a = int.Parse(Console.ReadLine());
 int b = a%2;
+
 if (b == 0)
 {
     Console.WriteLine("Четное число");
