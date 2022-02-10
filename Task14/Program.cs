112c1bdb515b9741dd7999cc941f7c6b508bb739
Maxim <the.buxta@yandex.ru>
2022-02-10 20:04:14 +0300

    отредактировал домашку

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,5 +1,3 @@
 ﻿// Найти третью цифру числа или сообщить, что её нет
-Random rand = new Random();
-int a = rand.Next(100,999);
-int b = a/10;
-Console.WriteLine("Третья цифра" + a + " = " + b);
+
+// пока думаю
